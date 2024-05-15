683f7281d1bc8014d0ed50ab47163e9fa081b3eb
manchilop <manuchicalopez@gmail.com>
2024-05-15 20:51:57 +0200

    created repository package with the interfaces for notice and user

---
+++ src/main/java/com/example/noticeboard/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package com.example.noticeboard.repository;
+
+import com.example.noticeboard.model.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
