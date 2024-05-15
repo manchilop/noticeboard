3d14c8e4e773979913443eeda8d5169767788ebf
manchilop <manuchicalopez@gmail.com>
2024-05-15 21:07:49 +0200

    security configuration with the Spring Security dependency

---
+++ src/main/java/com/example/noticeboard/service/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.noticeboard.service;
+
+import com.example.noticeboard.model.User;
+import com.example.noticeboard.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.isAdmin() ? "ADMIN" : "USER"))
+        );
+    }
+}
